f0b9a57dff866272a70c979f71a67869607c8e0d
Darciechapman <darciechapman96@gmail.com>
2020-07-23 16:14:36 +0800

    add navbar in component folder

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link as domLink } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+function AppNavbar() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <Navbar bg='dark' variant='dark' expand='lg'>
+            <Container fluid>
+                <Navbar.Brand as={Link} to='/'>Google Books Search</Navbar.Brand>
+                <Navbar.Toggle aria-controls='navbar'/>
+                <Navbar.Collapse id='navbar'>
+                    <Nav className='ml-auto'> 
+                        <Nav.Link as={Link} to='/'>Search For Books</Nav.Link>
+                        <Nav.Link as={Link} to='/saved'>See Saved Books</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default AppNavbar;
