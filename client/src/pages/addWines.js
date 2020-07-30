02989c5c711f1ced9094e331582fd1b455e0e3eb
Darciechapman <darciechapman96@gmail.com>
2020-07-30 22:04:25 +0800

    star rating testing

--- client/src/pages/addWines.js
+++ client/src/pages/addWines.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import { Col, Form, Button } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Col, Form, Button, Rater } from 'react-bootstrap';
+import Rater from 'react-rater'
+import 'react-rater/lib/react-rater.css'
+// import { render } from 'react-dom';
+
+// ract rating component ===  https://www.youtube.com/watch?v=eDw46GYAIDQ
 
 //import context for global state
 // import SavedWineContext from '../utils/SavedWineContext';
@@ -7,27 +12,45 @@ import { Col, Form, Button } from 'react-bootstrap';
 //import getSavedWines and deleteWine from API file
 // import * as API from '../utils/API';
 
+// class App extends Component {
+//     render() {
+//         return (
+//             <StarRating
+//                 defaultValue={5}
+//                 min={0}
+//                 max={5}
+//                 step={0.5} />
+//         )
+//     }
+// }
+render() {
+    return (<Rater total={5} rating={2} />)
+}
+
+
 function AddWines() {
+
     //<form onSubmit={handleFormSubmit}>
     return (
         <Form>
             <Form.Row>
                 <Col xs={12} md={8}>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
+                    <Form.Group controlId="wineName">
+                        <Form.Label>Wine name</Form.Label>
+                        <Form.Control type="name" placeholder="Enter wine name" />
                     </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                    <Form.Group controlId="wineYear">
+                        <Form.Label>Wine year</Form.Label>
+                        <Form.Control type="name" placeholder="Enter wine year" />
+                    </Form.Group>
+                    <Form.Group controlId="wineDescription">
+                        <Form.Label>Wine description</Form.Label>
+                        <Form.Control type="name" placeholder="Enter wine description" />
                     </Form.Group>
                     
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+                    <Form.Group controlId="wineRating">
+                        <Rater onRate={({rating}) => {}}/>
+                        <Rater rating={4} total={5} />
                     </Form.Group>
                 </Col>
                 <Col xs={12} md={4}>
@@ -38,4 +61,4 @@ function AddWines() {
     )
 }
 
-export default AddWines;
+export default AddWines 
